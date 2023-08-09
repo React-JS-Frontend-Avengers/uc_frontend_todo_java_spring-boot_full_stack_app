ee1f5e9143ef28cf9e9bfbdeb39050bca664eeb2
quantum_adrenaline_1 <yogiraj.chitam@gmail.com>
2023-08-09 21:36:11 +0530

    Updated the 'IMPROVED' Counter app implementation with JS styling for components
    Created an updated version of 'IMPROVED' Counter app implementation using Class component by updating the CSS styling for components with JavaScript styling

---
+++ U-C_TODO_frontend/src/course_learning/counter/JS-style-for-modularity-improved-code/component/App/App.style.js
@@ -0,0 +1,22 @@
+export default {
+  operationResult: {
+    display: "flex",
+    justifyContent: "center",
+    fontSize: 220,
+    marginTop: 15,
+    marginBottom: 0,
+  },
+  resetButtonDiv: {
+    display: "flex",
+    justifyContent: "center",
+  },
+  resetButton: {
+    width: 305,
+    height: 75,
+    marginTop: 15,
+    backgroundColor: "red",
+    color: "white",
+    fontSize: 21,
+    border: "none",
+  },
+};
---
+++ U-C_TODO_frontend/src/course_learning/counter/JS-style-for-modularity-improved-code/component/Button/Button.style.js
@@ -0,0 +1,10 @@
+export default {
+  operationButton: {
+    width: 150,
+    height: 55,
+    backgroundColor: "green",
+    border: "none",
+    color: "white",
+    fontSize: 18,
+  },
+};
