37b7cf684d5739d7f61e66f91dd35384b2ca0176
quantum_adrenaline_1 <yogiraj.chitam@gmail.com>
2023-08-09 21:36:11 +0530

    Updated the 'IMPROVED' Counter app implementation with JS styling for components
    Created an updated version of 'IMPROVED' Counter app implementation using Class component by updating the CSS styling for components with JavaScript styling

--- U-C_TODO_frontend/src/App.jsx
+++ U-C_TODO_frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import Counter from "./course_learning/counter/IMPROVED_class_component_application/index.component.jsx";
+import Counter from "./course_learning/counter/JS-style-for-modularity-improved-code/index.component.jsx";
 import "./App.css";
 
 export default class App extends Component {
